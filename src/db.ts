b90cc18083824e5b17f9cf49933e6421e071477f
Myra Guerrier <myra.jia.guerrier@gmail.com>
2023-06-27 22:29:43 -0400

    Allow SQLite DB path to be configured

--- src/db.ts
+++ src/db.ts
@@ -1,14 +1,17 @@
 
 import { DataSource } from 'typeorm'
+import {join} from 'path'
 
 import { Player } from './entities/Player'
 import { Vessel } from './entities/Vessel'
 import { Competition } from './entities/Competition'
 import { Session } from './entities/Session'
 
+const DB_PATH = process.env.SQL_DB_PATH ?? '/opt/hangar-api/data'
+
 export const db = new DataSource({
 	type: 'sqlite',
-	database: 'dev.sqlite3',
+	database: join(DB_PATH, 'dev.sqlite3'),
 	entities: [Player, Vessel, Competition, Session],
 	logging: true,
 	//synchronize: IS_DEV,
